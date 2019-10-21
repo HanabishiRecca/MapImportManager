f9f5ae3f1cd6b4c97a6e0925a2b15f7f12dd01de
Hanabishi <13597663+HanabishiRecca@users.noreply.github.com>
2019-10-22 01:18:55 +0500

    Version bump

--- MapImportManager/Properties/AssemblyInfo.cs
+++ MapImportManager/Properties/AssemblyInfo.cs
@@ -3,4 +3,4 @@
 [assembly: AssemblyTitle("Map Import Manager")]
 [assembly: AssemblyProduct("Warcraft III Map Import Manager")]
 [assembly: AssemblyCopyright("Hanabishi")]
-[assembly: AssemblyVersion("1.23.0.0")]
+[assembly: AssemblyVersion("1.24.0.0")]
